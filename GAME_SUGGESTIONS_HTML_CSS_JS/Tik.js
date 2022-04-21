c6f94fb77839c30062e8353a06bd373169db0155
vidit <vidittyagi200@gmail.com>
2022-04-21 13:35:19 +0530

    Comments Complete

--- GAME_SUGGESTIONS_HTML_CSS_JS/Tik.js
+++ GAME_SUGGESTIONS_HTML_CSS_JS/Tik.js
@@ -1,16 +1,44 @@
+//This event will be fired as soon as the DOM is loaded
 window.addEventListener('DOMContentLoaded', () => {
+    //These are selectors that we are bringing from html
+    //Take an array of tiles
     const tiles = Array.from(document.querySelectorAll('.tile'));
+    //take playerDisplay span which displays which player's turn is it 
     const playerDisplay = document.querySelector('.display-player');
+    //Reset button resets the game to initail state
     const resetButton = document.querySelector('#reset');
+    /*
+    announcer announces which player won at end of game 
+    this is hidden in the starting and will become visible on UI
+    when the game is ended. 
+    */
     const announcer = document.querySelector('.announcer');
 
+    //We will create an array board which will act as board for us 
     let board = ['', '', '', '', '', '', '', '', ''];
+    /*
+    Maintain a variable currentPlayer that will help us to know 
+    which player's turn is it zero(0) or Cross(X)
+    */ 
     let currentPlayer = 'X';
+
+    //This isGameActive variable will help us to know if user is still playing game or not 
     let isGameActive = true;
 
+    //These variables will help us to understand state of players at end of game
     const PLAYERX_WON = 'PLAYERX_WON';
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
+
+
+
+    /*
+    MAIN CODE START: - 
+    1. First function[tiles.forEach()] = start reading the code from this function.
+    This functoion is on 131 line no in this file
+    */
+
+
 
 
     /*
@@ -18,8 +46,8 @@ window.addEventListener('DOMContentLoaded', () => {
         [0] [1] [2]
         [3] [4] [5]
         [6] [7] [8]
+        This is an array of array which will dictate winning conditions for players
     */
-
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -31,6 +59,14 @@ window.addEventListener('DOMContentLoaded', () => {
         [2, 4, 6]
     ];
 
+    /*
+    In this function we are checking 
+    if at this certain position of x,y coordinate the user has won or not
+    By putting a for loop and checking in that certain row (a,b,c) 
+    if all is x or all is 0 then make roundWon true
+    and later check if roundWon is true then make Playerxwon variable announce or display
+
+    */
     function handleResultValidation() {
         let roundWon = false;
         for (let i = 0; i <= 7; i++) {
@@ -53,6 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
+        //If no empty string is left then display Tie
     if (!board.includes(''))
         announce(TIE);
     }
@@ -60,10 +97,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const announce = (type) => {
         switch(type){
             case PLAYERO_WON:
-                announcer.innerHTML = '<span style="color:red;" class="playerO">Player Won O</span>';
+                announcer.innerHTML = '<span class="playerO">Player Won O</span>';
                 break;
             case PLAYERX_WON:
-                announcer.innerHTML = '<span style="color:red;" class="playerX">Player X Won</span>';
+                announcer.innerHTML = '<span class="playerX">Player X Won</span>';
                 break;
             case TIE:
                 announcer.innerText = 'Tie';
@@ -71,7 +108,7 @@ window.addEventListener('DOMContentLoaded', () => {
         announcer.classList.remove('hide');
     };
 
-    
+
     const isValidAction = (tile) => {
         if (tile.innerText === 'X' || tile.innerText === 'O'){
             return false;
@@ -91,8 +128,30 @@ window.addEventListener('DOMContentLoaded', () => {
         playerDisplay.classList.add(`player${currentPlayer}`);
     }
 
-    const userAction = (tile, index) => {
-        if(isValidAction(tile) && isGameActive) {
+    /*
+    User Action function takes 2 parameters
+    tile div and index of tile div.
+    1. Then this function calls isValidAction() function 
+    to check if action by user is valid or not.
+    By checking if user is not again clicking on tile 
+    which is already filled with 0 or X.
+
+    2. Then it changes the tile state on UI
+    according to the current player that is playing either to 0 or X
+    
+    3. So in third step add current player classList to tile 
+    we are not using if condition here we are using template string literal
+    4. Then call updateBoard() function to update board with the current player's activity.
+    5. Then we will check that we have winner or not using handleResultValidation()
+    6. Then we have change player method helps in changing player's turn
+    for example : - 
+    if X player's turn is going on, then X has played 
+    now we have to change turn to 0 then 0 will play
+    This is acheived by removing x from playerDisplay ClassList
+    and adding 0 to it.
+    */
+    const userAction = (tile, index) =>{
+        if(isValidAction(tile) && isGameActive){
             tile.innerText = currentPlayer;
             tile.classList.add(`player${currentPlayer}`);
             updateBoard(index);
@@ -117,6 +176,12 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    /*
+    Used JavaScript ForEach function which will take each tile div
+    and attach a click event on it as soon as user clicks on tile,
+    an userAction function will be called where tile div 
+    and index of that tile will be given as parameters 
+    */
     tiles.forEach( (tile, index) => {
         tile.addEventListener('click', () => userAction(tile, index));
     });
