2785fab3aa6cdef408f45d5b91bb3ff1a59cb1dd
vidit <vidittyagi200@gmail.com>
2022-04-22 16:17:43 +0530

    AJAX CALL project css fixed

--- AJAX/script.js
+++ AJAX/script.js
@@ -15,7 +15,7 @@ async function  dataAPI(e) {
     for (i = 0;i < data.results.length;i++){
  
     let gameCard = `
-    <div class="card" style="width: 20rem;">
+    <div class="card" style="width:20rem;margin-top:10rem;">
      <img src='${data.results[i].background_image}' class="card-img-top">
      <div class="card-body">
      <h5 class="card-title">${data.results[i].name} </h5>
