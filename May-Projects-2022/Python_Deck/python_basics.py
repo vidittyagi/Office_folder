957a8f521149e0e90a37158a4055e96d07472835
vidit <vidittyagi200@gmail.com>
2022-05-11 12:50:07 +0530

    python file

--- May-Projects-2022/Python_Deck/python_basics.py
+++ May-Projects-2022/Python_Deck/python_basics.py
@@ -1,4 +1,76 @@
 
+# a = 50
+# b = 100
+# c = 200
+# print((a < 1000 and b > 50) and c > 10)
+
+
+#Logical operators
+# a = 5
+# print(a < 5 and a < 50)
+
+# print(a < 5 and a < 4)
+
+# print(not(a < 5 and a < 50))
+
+
+
+
+
+# #Comparison operators
+# print(5 == 10)
+# print(5 != 10)
+# print(10 > 5)
+# print(5 < 10)
+# print(5 >= 5)
+# print(5 <= 5)
+
+
+
+
+
+
+
+
+
+# #Assignment operators
+# a = 5
+# print(a)
+# print(a + 5)
+# print(a - 5)
+# print(a * 10)
+# print(a / 2)
+# print(a % 2)
+# print(a // 3)
+# print(a ** 3)
+
+
+
+
+
+# print(variable1)
+# variable1 = 25
+# print(variable1)
+
+# age = input("Enter your age in years :")
+# ageInDays = 365 * age
+# print(ageInDays)
+
+
+# #Arithmatic operators
+# #ADDITION
+# print(25 + 25)
+# #SUBTRACTION
+# print(50 - 25)
+# #MULTIPLICATION
+# print(50 * 50)
+# #DIVISION
+# print(100 / 2)
+
+
+
+
+
 # dalMakhani = "$25"
 # dosa = "$50"
 # bread = "$10"
@@ -29,11 +101,11 @@
 
 
 
-nameList = ["dalMakhani","dosa","bread","chocolateShake"]
-pricesList = ["$25", "$50", "$10", "$100"]
+# nameList = ["dalMakhani","dosa","bread","chocolateShake"]
+# pricesList = ["$25", "$50", "$10", "$100"]
 
-for element in range(1,4) : {
-    print(nameList[element]  + " " + pricesList[element])
-}
+# for element in range(1,4) : {
+#     print(nameList[element]  + " " + pricesList[element])
+# }
 
 
