230bc1b7252aac5e0c126002b46e57679c82b212
vidit <vidittyagi200@gmail.com>
2022-05-19 09:39:50 +0530

    Key added to map method

--- May-Projects-2022/Authentication/src/Components/SignUp/SignUp.jsx
+++ May-Projects-2022/Authentication/src/Components/SignUp/SignUp.jsx
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 const SignUp = ()=>{
     const[APIData,setAPIData] = useState([]);
     const getData = async ()=>{
+        //Advice API
         //query = [life, doctor]
         let response = await fetch('https://api.adviceslip.com/advice/search/life');
         let data = await response.json();
         console.log(data.slips);
         setAPIData(data.slips);
+
+        //Pokemon API
+        // let response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+        // let data = await response.json();
+        // console.log(data.species.url);
     }
     
     useEffect(()=>{
@@ -19,7 +25,7 @@ const SignUp = ()=>{
             {
                 APIData.map((val)=>{
                     return (
-                        <h1>{val.advice}</h1>
+                        <h1 key={val.id}>{val.advice}</h1>
                     )
                 })
             }
