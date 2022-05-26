37fd598dc02aef9e82f1977f3d14933e4fa87b28
vidit <vidittyagi200@gmail.com>
2022-05-26 14:42:56 +0530

    backend project and css animation

---
+++ May-Projects-2022/Image_Storage_JSON_Api/backend.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+mongoose.connect('mongodb://localhost:27017/Image_Storage');
+const connection = mongoose.connection;
+
+const imageSchema = new mongoose.Schema({
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+    },
+    url:{
+        type:String,
+        required:true
+    }
+});
+
+const imageModel = new mongoose.model("imageModel",imageSchema);
+
+app.post("/addImage",(req,res)=>{
+    let imageObj = {
+        id:req.body.id,
+        name:req.body.name,
+        url:req.body.url
+    }
+
+    let doc = new imageModel(imageObj);
+    doc.save((err)=>{
+        if(err)
+        res.send('Data Not Received');
+        else
+        res.send('Success');
+    });
+});
+
+app.get("/getImage", (req, res)=>{
+    imageModel.find({},(err,result)=>{
+        if(err)
+            console.log(err);
+        else
+            res.send(result);
+    });
+});
+
+
+app.listen(port, ()=>{console.log(`Hello from backend ${port}`)});
---
+++ May-Projects-2022/Image_Storage_JSON_Api/src/Components/Displayphoto/Displayphoto.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import "./displayphoto.css";
+
+const Displayphoto = () => {
+    const[arrayOfData,setArrayOfData] = useState([]);
+
+    const handleData = async()=> {
+        try{
+            let response = await fetch('http://localhost:5000/getImage');
+            let data = await response.text();
+            console.log(data);
+            arrayOfData.push(data);
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        handleData();
+    },[]);
+
+    return(
+        <div className="containerDisplayphoto">
+            <section className='sectionDP'>
+                <h5>Id:1</h5>
+                <h3>Name</h3>
+                <img 
+                style={{height:"100%",width:"100%"}}
+                src="https://cdn.pixabay.com/photo/2016/11/30/15/00/lighthouse-1872998_1280.jpg"
+                alt="Image not found"
+                />
+            </section>
+        </div>
+     );
+}
+ 
+export default Displayphoto;
