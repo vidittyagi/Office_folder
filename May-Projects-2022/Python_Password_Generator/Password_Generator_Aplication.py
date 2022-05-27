6f1e4167da6822a5c2d24417a68a31e2dd27e907
vidit <vidittyagi200@gmail.com>
2022-05-28 00:33:13 +0530

    python Password generator completed

--- May-Projects-2022/Python_Password_Generator/Password_Generator_Aplication.py
+++ May-Projects-2022/Python_Password_Generator/Password_Generator_Aplication.py
@@ -1,61 +1,81 @@
-# print("---------------------------Password Generator---------------------------")
-# login = False
-# passwordEncodedDict = {}
-# username = input("Enter Username ")
-# password = input("Enter Password ")
-
-# if(username != "xyz@gmail.com" and password != "1234"):
-#     print("Wrong Username or Password")
-#     login = True
-# else:
-
-login=True
+print("---------------------------Password Generator---------------------------")
+#Maintain a variable login to check whether user is logged in or not 
+login = False
+#Maintain a dictionary to store values of application names and password entered by user
 passwordEncodedDict = {}
+username = input("Enter Username ")
+password = input("Enter Password ")
+print("")
 
-while login:
-    print("1. Enter Password")
-    print("2. View Password")
-    print("3. Delete Password")
-    print("4. Logout")
-    print("       ")
+#Check if user is valid or not
+if(username != "xyz@gmail.com" or password != "1234"):
+    print("Wrong Username or Password")
+else:
+    #Make login true if username and password is valid 
+    login = True
+    #Apply while loop until login is true. 
+    #This funtionality is used to show the options again and again to user until he/she logs out by option 4
+    while login:
+        print("1. Enter Password")
+        print("2. View Password")
+        print("3. Delete Password")
+        print("4. Logout")
+        print("       ")
 
-    optionSelected = input("Select an option ")
-    optionSelected = int(optionSelected)
+        optionSelected = input("Select an option ")
+        optionSelected = int(optionSelected)
 
-    if(optionSelected == 1):
-        newApplicationName = input("Enter application name")
-        newPassword = input("Enter Password")
-        passwordEncoded = newPassword+'2'
-        passwordEncodedDict[newApplicationName] = passwordEncoded
-    
-    elif(optionSelected == 2):
-        length = len(passwordEncodedDict)
-        if(length == 0):
-            print("Please enter a password first")
-            print(" ")
+        #option 1 is selected
+        if(optionSelected == 1):
+            newApplicationName = input("Enter application name ")
+            newPassword = input("Enter Password ")
+            print("")
+            #Encode password by adding any string to the end of it
+            passwordEncoded = newPassword+'2'
+            #Put password in dictionary mapping application name to password entered 
+            passwordEncodedDict[newApplicationName] = passwordEncoded
+
+        #option 2 is selected
+        elif(optionSelected == 2):
+            #check if dictionary length is 0 then there is nothing to show to user
+            length = len(passwordEncodedDict)
+            if(length == 0):
+                print("Please enter a password first")
+                print(" ")
+            else:
+                for key in passwordEncodedDict:
+                    #extract password using key value from dictionary
+                    string = passwordEncodedDict[key]
+                    #remove character from last of the password string
+                    passwordDecoded = string[:-1]
+                    print("Application name is " + key + " and password is " + passwordDecoded)
+                    print("")
+
+            #option 3 is selected
+        elif(optionSelected == 3):
+            keyInDictionary = input("Enter application name which needs to be deleted ")
+            #search for application name in python dictionary
+            if(keyInDictionary in passwordEncodedDict):
+                #Maintain a password decoded variable which will store original password value by decoding it removing 2 from last of the string
+                passwordDecoded = ""
+                #Apply for loop for searching
+                for key in passwordEncodedDict:
+                    if(key == keyInDictionary):
+                        string = passwordEncodedDict[key]
+                        #remove character
+                        passwordDecoded = string[:-1]
+                #pop out password from dictionary
+                deletedPass = passwordEncodedDict.pop(keyInDictionary)
+                print("Password for " + keyInDictionary + ":" + passwordDecoded + " is deleted")
+                print("")
+            else:
+                print("Invalid Application Name")
+                print("")
+
+        elif(optionSelected == 4):
+            login = False
+            print("Logged Out")
         else:
-            for key in passwordEncodedDict:
-                string = passwordEncodedDict[key]
-                #remove character
-                passwordDecoded = string[:-1]
-                print(key + " " + passwordDecoded)
-    
-    elif(optionSelected == 3):
-        keyInDictionary = input("Enter application name which needs to be deleted")
-        if(keyInDictionary in passwordEncodedDict):
-            passwordDecoded = ""
-            for key in passwordEncodedDict:
-                string = passwordEncodedDict[key]
-                #remove character
-                passwordDecoded = string[:-1]
-            deletedPass = passwordEncodedDict.pop(keyInDictionary)
-            print("Password for " + keyInDictionary + ":" + passwordDecoded + " is deleted")
-        else: print("Invalid Application Name")
-    
-    elif(optionSelected == 4):
-        login = False
-        print("Logged Out")
-    else:
-        print("Invalid option")
+            print("Invalid option")
 
 
